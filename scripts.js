a5a20f9f18e42884ac3d411a9c7c7f0bc0d421b1
aldo aldaco <aldo_aldaco@hotmail.com>
2017-07-19 13:06:37 -0500

    Add functin to paint array sugestions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 'use strict';
 
 var searchList = [];
+
+function paintArray(){
+    //var div = document.getElementById('dropdown');
+    var ul = document.getElementById("dropdown");
+    if(searchList.length=='0'){
+        console.log('No hay sugerencias');
+    } else {
+        for(i of searchList){
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(i));
+            ul.appendChild(li);
+        }
+    }
+    //return list;
+}
+
+function clearPaint(){
+    var ul = document.getElementById("dropdown");
+    ul.innerHTML='';
+}
