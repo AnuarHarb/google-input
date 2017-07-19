5b8c2c3bddd75b10251467bf24203ff13a0ff6f5
israelDuranV <30297088+israelDuranV@users.noreply.github.com>
2017-07-19 13:50:23 -0500

    validamos antes de agregar al array

--- scripts.js
+++ scripts.js
@@ -3,11 +3,16 @@ var searchList = [];
 
 function agregarOption()
 {
-	var busca = document.getElementById("busqueda").value;
-	searchList.push(busca);  
+	var busca = document.getElementById("google-input").value;
+
+        if (searchList.indexOf(busca) < 0 ) {
+            console.log("El elemento no se encuentra y se agregara");
+						searchList.push(busca);
+        }else{
+					console.log("El elemento"+ busca +" ya se encuentra");
+				}
 	console.log(searchList);
 }
-
 
 function paintArray(){
     //var div = document.getElementById('dropdown');
