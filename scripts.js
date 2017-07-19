cdb855866065a2afe61e15aa1b07c4b177d05e9a
anuarharb <anuarharb@hotmail.com>
2017-07-19 13:55:09 -0500

    remove function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 'use strict';
 
-var searchList = [];
+var searchList = ["1","2","3"];
+
+var options = document.getElementsByClassName("removeButton");
+for (let option of options) {
+  option.addEventListener("click",
+    removeOptionFromList);
+}
+
+function removeOptionFromList() {
+  console.log(this);
+  var option = this.value;
+  var i = searchList.indexOf(option);
+
+  if(i != -1) {
+  	searchList.splice(i, 1);
+    console.log(searchList);
+  } else {
+    console.log('no hay match');
+  }
+}
