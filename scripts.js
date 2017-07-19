1fd52c129e82d62e1397fcfa12033ec1778e7e26
aldo aldaco <aldo_aldaco@hotmail.com>
2017-07-19 13:52:17 -0500

    -

--- scripts.js
+++ scripts.js
@@ -6,11 +6,17 @@ function paintArray(){
     //var div = document.getElementById('dropdown');
     var ul = document.getElementById("dropdown");
     if(searchList.length=='0'){
-        console.log('No hay sugerencias');
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode('No hay sugerencias'));
+        ul.appendChild(li);
     } else {
         for(i of searchList){
+            var boton = document.createElement("button");
+            boton.setAttribute('class','removeButton');
+            boton.innerHTML='X';
             var li = document.createElement("li");
             li.appendChild(document.createTextNode(i));
+            li.appendChild(boton);
             ul.appendChild(li);
         }
     }
