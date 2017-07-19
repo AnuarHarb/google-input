5a135978e8bb84cc5130075e5357505086b72449
israelDuranV <30297088+israelDuranV@users.noreply.github.com>
2017-07-19 13:07:22 -0500

    Agrege la función agregar Option
    Se agregan opciones en el array a partir de un registro agregagdo
    en el input de buscar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 'use strict';
 
-var searchList = [];
+var searchList = ["algo","otra cosa","otra mas"];
+
+
+function agregarOption()
+{
+	var busca = document.getElementById("busqueda").value;
+	searchList.push(busca);  
+	console.log(searchList);
+}
+
